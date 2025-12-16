705159e8e078fd223892e80915fb5a43977c61cb
Vxrpenter <vxrpenter@noreply.codeberg.org>
2025-12-16 19:51:08 +0100

    Changing the file name of the templates

--- templates/git/install-templates.sh
+++ templates/git/install-templates.sh
@@ -6,7 +6,7 @@ templateContributing="https://codeberg.org/Vxrpenter/dotfiles/raw/branch/main/te
 repo_location="$1"
 
 printf "Installing Templates...\n"
-curl --create-dirs -O --output-dir $repo_location "$templateCodeOfConduct"
-curl --create-dirs -O --output-dir $repo_location "$templateContributing"
+curl --create-dirs -O --output-dir $repo_location/CODE_OF_CONDUCT.md "$templateCodeOfConduct"
+curl --create-dirs -O --output-dir $repo_location/CONTRIBUTING.md "$templateContributing"
 printf "Templates have been installed."
 exit 1
